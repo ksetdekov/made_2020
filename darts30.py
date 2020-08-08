08478bdd7f19d179023dc767b7fc5bfce05a33a0
ksetdekov <ksetdekov@gmail.com>
2020-08-08 13:59:48 +0300

    added infinite wait?

--- darts30.py
+++ darts30.py
@@ -2,10 +2,11 @@
 # 1
 # 8 2
 # 2 3 4 5 -30 6 -1 2
-data = input('input')
-result = []
-count = int(data[0])
-for i in range(0, count):
-    # print(i)
-    result.append(12)
-print(*result, sep='\n')
+while True:
+    data = input('input')
+    result = []
+    count = int(data[0])
+    for i in range(0, count):
+        # print(i)
+        result.append(12)
+    print(*result, sep='\n')
